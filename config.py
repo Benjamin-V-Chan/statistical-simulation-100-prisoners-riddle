df3e42780cb627eec2b200415689e784c6d57d1e
Benjamin Chan <benjamin.v.chan@gmail.com>
2024-10-31 11:17:01 -0700

    config.py complete with parameters

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
 # Stores configuration parameters for the simulation, such as number of prisoners and trials
+
+NUM_TRIALS = 100 # Number of trials for each strategy
+NUM_PRISONERS = 100
+BOX_LIMIT = 50
